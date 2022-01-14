1e93ec0280881ddfd59351361e591cd2a182e897
asyrul21 <asyrulhafetzy.21@gmail.com>
2022-01-14 16:41:48 +0800

    minor update

--- API/triggeredEventService.js
+++ API/triggeredEventService.js
@@ -3,7 +3,7 @@ const { default: axios } = require("axios");
 const serviceAPI = "https://xendit-trial-integration.herokuapp.com/api/";
 const EventsService = {
   updateStatus: async (id, status) => {
-    const { data } = await axios.put(`${serviceAPI}/events/${id}`, {
+    const { data } = await axios.put(`${serviceAPI}events/${id}`, {
       status,
     });
     return data;
